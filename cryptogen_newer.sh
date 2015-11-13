7b932d7efe9dde9b7a6684c96205e59448a425b3
JarathLoki <JarathLoki@gmail.com>
2015-11-13 11:50:47 -0500

    derp

--- cryptogen_newer.sh
+++ cryptogen_newer.sh
@@ -300,6 +300,9 @@ function DownloadBase() {
   # Downloading latest stage3.
   # TODO: Check .DIGESTS file.
   #wget -P $NEWROOT "$AUTOBUILDS""$LATEST_STAGE3"
+  mkdir /mnt/gentoo
+  mkdir /mnt/gentoo/proc
+  mkdir /mnt/gentoo/dev
   wget http://gentoo.osuosl.org/releases/amd64/autobuilds/current-stage3-amd64-hardened/stage3-amd64-hardened-20151112.tar.bz2
   # Unpacking stage3. Call the shell to try to untar the unit
   #/bin/sh -c tar xvjpf $NEWROOT/stage3-'*'.tar.bz2 -C $NEWROOT | dialog --infobox "Unpacking stage3..." 3 30
